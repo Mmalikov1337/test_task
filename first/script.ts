0bbfa793576e8ddeebaec2e4531d5eb67154812b
Mmalikov1337 <grelexis1337@inbox.ru>
2021-09-23 16:48:26 +0600

    add second singular

--- first/script.ts
+++ first/script.ts
@@ -66,18 +66,7 @@ class Conjugateable {
 		console.log("Спряжение не определено", { word: this.word });
 		return { conjugation: 0, end: wordEnd };
 	}
-	conjugateSingularFirst(end: string): string {
-		// спрягать 1л ед.ч (звонить ->(я)звоню)
-		const separated = this.word.replace(end, ""); //убрать конец(звонить ->звон)]
-		const hissing = "жчшщ".split("");
-		const withEditedLast = this.editLastByEndFirst(separated, end);
 
-		// console.log({ separated });
-		if (hissing.includes(withEditedLast[withEditedLast.length - 1])) {
-			return withEditedLast + "у";
-		}
-		return withEditedLast + "ю";
-	}
 	private editLastByEndFirst(body: string, end: string): string {
 		if (end === "ить") {
 			// console.log({ last: body[body.length - 1] });
@@ -126,15 +115,27 @@ class Conjugateable {
 		}
 		return body;
 	}
+	private conjugateSingularFirst(end: string): string {
+		// спрягать 1л ед.ч (звонить ->(я)звоню)
+		const separated = this.word.replace(end, ""); //убрать конец(звонить ->звон)]
+		const hissing = "жчшщ".split("");
+		const withEditedLast = this.editLastByEndFirst(separated, end);
+
+		// console.log({ separated });
+		if (hissing.includes(withEditedLast[withEditedLast.length - 1])) {
+			return withEditedLast + "у";
+		}
+		return withEditedLast + "ю";
+	}
 	private conjugateSingularSecond(end: string, conjugation: number): string {
 		// спрягать 2л ед.ч (звонить ->(я)звоню)
 		const separated = this.word.replace(end, ""); //убрать конец(звонить ->звон)]
 		// const withEditedLast = editLastByEnd(separated, end);
-		if (conjugation === 2) {
+		if (separated[separated.length - 1] === "е") {
 			// Глаголы с шипящим на конце всегда пишутся с мягким знаком
-			return separated + "ишь";
+			return separated + "ешь";
 		}
-		return separated + "ешь";
+		return separated + "е" + "ешь";
 	}
 }
 const exceptions = [
@@ -313,32 +314,119 @@ const exceptions = [
 // console.log("цепенеть -> ", conjugate("цепенеть", "я"));
 // console.log("ядренеть -> ", conjugate("ядренеть", "я"));
 
-//2 л
-console.log("звонить ->", conjugate("звонить", "ты"));
-console.log("вывозить ->", conjugate("вывозить", "ты"));
-console.log("любить ->", conjugate("любить", "ты"));
-console.log("клеить ->", conjugate("клеить", "ты"));
-console.log("лепить ->", conjugate("лепить", "ты"));
-console.log("ставить ->", conjugate("ставить", "ты"));
-console.log("бурить ->", conjugate("бурить", "ты"));
-console.log("копить ->", conjugate("копить", "ты"));
-console.log("чертить ->", conjugate("чертить", "ты"));
-console.log("косить ->", conjugate("косить", "ты"));
-console.log("коптить ->", conjugate("коптить", "ты"));
-console.log("водить ->", conjugate("водить", "ты"));
-console.log("верить ->", conjugate("верить", "ты"));
-console.log("жалить ->", conjugate("жалить", "ты"));
-console.log("хвалить ->", conjugate("хвалить", "ты"));
-console.log("молить ->", conjugate("молить", "ты"));
-console.log("вялить ->", conjugate("вялить", "ты"));
-console.log("винить ->", conjugate("винить", "ты"));
-console.log("водить ->", conjugate("водить", "ты"));
-console.log("возить ->", conjugate("возить", "ты"));
-console.log("вопить ->", conjugate("вопить", "ты"));
-console.log("помнить ->", conjugate("помнить", "ты"));
-console.log("готовить ->", conjugate("готовить", "ты"));
-console.log("тревожить ->", conjugate("тревожить", "ты"));
+//2 л ед
+// console.log("звонить ->", conjugate("звонить", "ты"));
+// console.log("вывозить ->", conjugate("вывозить", "ты"));
+// console.log("любить ->", conjugate("любить", "ты"));
+// console.log("клеить ->", conjugate("клеить", "ты"));
+// console.log("лепить ->", conjugate("лепить", "ты"));
+// console.log("ставить ->", conjugate("ставить", "ты"));
+// console.log("бурить ->", conjugate("бурить", "ты"));
+// console.log("копить ->", conjugate("копить", "ты"));
+// console.log("чертить ->", conjugate("чертить", "ты"));
+// console.log("косить ->", conjugate("косить", "ты"));
+// console.log("коптить ->", conjugate("коптить", "ты"));
+// console.log("водить ->", conjugate("водить", "ты"));
+// console.log("верить ->", conjugate("верить", "ты"));
+// console.log("жалить ->", conjugate("жалить", "ты"));
+// console.log("хвалить ->", conjugate("хвалить", "ты"));
+// console.log("молить ->", conjugate("молить", "ты"));
+// console.log("вялить ->", conjugate("вялить", "ты"));
+// console.log("винить ->", conjugate("винить", "ты"));
+// console.log("водить ->", conjugate("водить", "ты"));
+// console.log("возить ->", conjugate("возить", "ты"));
+// console.log("вопить ->", conjugate("вопить", "ты"));
+// console.log("помнить ->", conjugate("помнить", "ты"));
+// console.log("готовить ->", conjugate("готовить", "ты"));
+// console.log("тревожить ->", conjugate("тревожить", "ты"));
 
+console.log("подклеть -> ", conjugate("подклеть", "я"));
+console.log("подудеть -> ", conjugate("подудеть", "ты"));
+console.log("подюжеть -> ", conjugate("подюжеть", "ты"));
+console.log("пожалеть -> ", conjugate("пожалеть", "ты"));
+console.log("пожидеть -> ", conjugate("пожидеть", "ты"));
+console.log("покипеть -> ", conjugate("покипеть", "ты"));
+console.log("поколеть -> ", conjugate("поколеть", "ты"));
+console.log("полеветь -> ", conjugate("полеветь", "ты"));
+console.log("полететь -> ", conjugate("полететь", "ты"));
+console.log("полысеть -> ", conjugate("полысеть", "ты"));
+console.log("помелеть -> ", conjugate("помелеть", "ты"));
+console.log("помереть -> ", conjugate("помереть", "ты"));
+console.log("поноветь -> ", conjugate("поноветь", "ты"));
+console.log("попереть -> ", conjugate("попереть", "ты"));
+console.log("попотеть -> ", conjugate("попотеть", "ты"));
+console.log("порадеть -> ", conjugate("порадеть", "ты"));
+console.log("пореветь -> ", conjugate("пореветь", "ты"));
+console.log("поредеть -> ", conjugate("поредеть", "ты"));
+console.log("порусеть -> ", conjugate("порусеть", "ты"));
+console.log("порыжеть -> ", conjugate("порыжеть", "ты"));
+console.log("порябеть -> ", conjugate("порябеть", "ты"));
+console.log("поседеть -> ", conjugate("поседеть", "ты"));
+console.log("посереть -> ", conjugate("посереть", "ты"));
+console.log("посиветь -> ", conjugate("посиветь", "ты"));
+console.log("посидеть -> ", conjugate("посидеть", "ты"));
+console.log("посинеть -> ", conjugate("посинеть", "ты"));
+console.log("посипеть -> ", conjugate("посипеть", "ты"));
+console.log("посопеть -> ", conjugate("посопеть", "ты"));
+console.log("потереть -> ", conjugate("потереть", "ты"));
+console.log("поумнеть -> ", conjugate("поумнеть", "ты"));
+console.log("похудеть -> ", conjugate("похудеть", "ты"));
+console.log("пошипеть -> ", conjugate("пошипеть", "ты"));
+console.log("пошуметь -> ", conjugate("пошуметь", "ты"));
+console.log("презреть -> ", conjugate("презреть", "ты"));
+console.log("пригреть -> ", conjugate("пригреть", "ты"));
+console.log("призреть -> ", conjugate("призреть", "ты"));
+console.log("приодеть -> ", conjugate("приодеть", "ты"));
+console.log("приспеть -> ", conjugate("приспеть", "ты"));
+console.log("прогреть -> ", conjugate("прогреть", "ты"));
+console.log("прозреть -> ", conjugate("прозреть", "ты"));
+console.log("пропреть -> ", conjugate("пропреть", "ты"));
+console.log("простеть -> ", conjugate("простеть", "ты"));
+console.log("протлеть -> ", conjugate("протлеть", "ты"));
+console.log("прочнеть -> ", conjugate("прочнеть", "ты"));
+console.log("разодеть -> ", conjugate("разодеть", "ты"));
+console.log("разуметь -> ", conjugate("разуметь", "ты"));
+console.log("роговеть -> ", conjugate("роговеть", "ты"));
+console.log("розоветь -> ", conjugate("розоветь", "ты"));
+console.log("румянеть -> ", conjugate("румянеть", "ты"));
+console.log("сатанеть -> ", conjugate("сатанеть", "ты"));
+console.log("свербеть -> ", conjugate("свербеть", "ты"));
+console.log("свертеть -> ", conjugate("свертеть", "ты"));
+console.log("светлеть -> ", conjugate("светлеть", "ты"));
+console.log("свистеть -> ", conjugate("свистеть", "ты"));
+console.log("сиротеть -> ", conjugate("сиротеть", "ты"));
+console.log("скорбеть -> ", conjugate("скорбеть", "ты"));
+console.log("скрипеть -> ", conjugate("скрипеть", "ты"));
+console.log("скучнеть -> ", conjugate("скучнеть", "ты"));
+console.log("смердеть -> ", conjugate("смердеть", "ты"));
+console.log("смирнеть -> ", conjugate("смирнеть", "ты"));
+console.log("смотреть -> ", conjugate("смотреть", "ты"));
+console.log("смуглеть -> ", conjugate("смуглеть", "ты"));
+console.log("сожалеть -> ", conjugate("сожалеть", "ты"));
+console.log("соловеть -> ", conjugate("соловеть", "ты"));
+console.log("солодеть -> ", conjugate("солодеть", "ты"));
+console.log("солонеть -> ", conjugate("солонеть", "ты"));
+console.log("стемнеть -> ", conjugate("стемнеть", "ты"));
+console.log("стерпеть -> ", conjugate("стерпеть", "ты"));
+console.log("суроветь -> ", conjugate("суроветь", "ты"));
+console.log("твердеть -> ", conjugate("твердеть", "ты"));
+console.log("толстеть -> ", conjugate("толстеть", "ты"));
+console.log("трезветь -> ", conjugate("трезветь", "ты"));
+console.log("трухлеть -> ", conjugate("трухлеть", "ты"));
+console.log("трухтеть -> ", conjugate("трухтеть", "ты"));
+console.log("тускнеть -> ", conjugate("тускнеть", "ты"));
+console.log("тяготеть -> ", conjugate("тяготеть", "ты"));
+console.log("тяжелеть -> ", conjugate("тяжелеть", "ты"));
+console.log("углядеть -> ", conjugate("углядеть", "ты"));
+console.log("угрюметь -> ", conjugate("угрюметь", "ты"));
+console.log("устареть -> ", conjugate("устареть", "ты"));
+console.log("утерпеть -> ", conjugate("утерпеть", "ты"));
+console.log("холодеть -> ", conjugate("холодеть", "ты"));
+console.log("хорошеть -> ", conjugate("хорошеть", "ты"));
+console.log("храбреть -> ", conjugate("храбреть", "ты"));
+console.log("хрустеть -> ", conjugate("хрустеть", "ты"));
+console.log("цепенеть -> ", conjugate("цепенеть", "ты"));
+console.log("ядренеть -> ", conjugate("ядренеть", "ты"));
 function conjugate(word: string, pronoun: string) {
 	const instance = new Conjugateable(word);
 	return instance.conjugate(pronoun);
